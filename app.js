e4a20f6836acd4ade6fdefb4f0765a881d1a1e1f
Kyle Durigan <kyle.durigan@gmail.com>
2020-10-07 15:45:56 -0600

    Recieving response from bridge with scenes, lights etc.

--- app.js
+++ app.js
@@ -5,135 +5,211 @@ const inquirer = require("inquirer");
 const axios = require('axios');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+	res.send('Hello World!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-    lightingHomePrompt();
+	console.log(`Example app listening at http://localhost:${port}`)
+	lightingHomePrompt();
 })
 
 let isWindows = /^win/.test(process.platform); // use rawlist type for windows
 let listType;
 !isWindows ? listType = 'list' : listType = 'rawlist';
 
+// FIXME: GLOBAL VARIABLES FOR TESTING. MOVE TO DB ONCE FINISHED TESTING.
+let lightingData = [];
+
 function lightingHomePrompt() {
-    // console.log('lighting started');
-    // await checkCurrentStatus();
-    inquirer.prompt({
-        name: 'lightingHome',
-        type: listType,
-        message: 'Welcome to the lighting hub, what would you like to do?',
-        choices: [
-            'Control Lighting',
-            'Set Up Lights',
-            'Exit'
-        ]
-    }).then(answer => {
-        switch (answer.lightingHome) {
-            case 'Control Lighting':
-                getLightingData();
-                // getLightingDevices();
-                // controlLights();
-                break;
+	// console.log('lighting started');
+	// await checkCurrentStatus();
+	inquirer.prompt({
+		name: 'lightingHome',
+		type: listType,
+		message: 'Welcome to the lighting hub, what would you like to do?',
+		choices: [
+			'Control Lighting',
+			'Set Up Lights',
+			'Exit'
+		]
+	}).then(async answer => {
+		switch (answer.lightingHome) {
+			case 'Control Lighting':
+				await getLightingData();
+				await controlLights();
+				break;
 
-            case ' Set Up Lights':
-                break;
+			case ' Set Up Lights':
+				break;
 
-            case 'Exit':
-                app.end();
+			case 'Exit':
+				app.end();
 
-            default:
-                return console.log('Lighting Home Answer Not Selected', answer);
-
-        }
-    }).catch(err => console.error('ERROR IN LIGHTING HOME PROMPT: ', err))
+			default:
+				return console.log('Lighting Home Answer Not Selected', answer);
+		}
+	}).catch(err => console.error('ERROR IN LIGHTING HOME PROMPT: ', err))
 }
 
 async function getLightingData() {
-    const previousData = await checkForProfileData();
-    // CHECK TO SEE IF WE HAVE HUE LIGHTS
-    // DISCOVER LOCAL DEVICES ON NETWORK
-    let discoveryApp = 'https://discovery.meethue.com';
-    let discoveryData;
-    let hueUsername = 'Kdurg';
-    let username;
-    let coreURL;
+	const previousData = await checkForProfileData();
 
-    axios.get(discoveryApp)
-        .then(res => {
-            discoveryData = res.data;
-        })
-        .then(async () => {
-            if (discoveryData) {
-                console.log('RESPONSE: ', discoveryData);
-                username = await getHueUserName(discoveryData);
-            } else {
-                console.error('ERROR: UNABLE TO GET HUE BRIDGE DATA')
-            }
+	// PHILIPS HUE
+	// DISCOVER LOCAL DEVICES ON NETWORK
+	await getHueData()
+	.then(data =>{ lightingData.push(data)}) // FIXME: NOT ADDING TO THE ARRAY?
+	// if (await hueData) {
+	// 	lightingData.push(hueData)
+		console.log('LIGHTING DATA', lightingData)
+	// }
+	
 
-        })
-        .catch(err => console.error("ERROR WITH DISCOVERY APP: ", err.status))
 
-    if (!previousData) {
-        console.error('ERROR: NO PREVIOUS USER DATA', previousData);
-    } else {
-        console.log('PREVIOUS DATA CHECK: FINISHED', previousData);
-        await getHueData();
-    }
-}
-
-// IF WE DO NOT HAVE A USER ASSIGNED YET, WE NEED TO CREATE ONE
-async function getHueUserName(hueData) {
-    console.log('gggggg', hueData)
-    let usernameID = 'Me78u3OjDQfTnyAdMemLje9-J3uJqyQih-2NZHmL' // aGbX60RgWmgCVTf9BRevBfcZDmjjZ9xd1eow4F8V
-    if (hueData[0] && hueData[0].internalipaddress) {
-        let reqURL = `http://${hueData[0].internalipaddress}/api`;
-        // axios.get(reqURL)
-        //     .then(res=> {console.log('res  ', res)})
-        //     .catch(err=> console.error('ERROR GETTING USERNAME: ', err))
-
-        let data = {
-            "devicetype": "my_hue_app#kylelaptop"
-        }
-
-        // // SEND HUE BRIDGE NEW USERNAME
-        // axios.post(reqURL, data)
-        //     .then(res => { 
-        //         console.log('res  ', res.data) 
-        //         if (res.success) {
-        //             console.log('Success!', res);
-        //             usernameID = res.username;
-        //         } else { console.log('USER POST ERROR: ', res)}
-        //     })
-        //     .catch(err => console.error('ERROR GETTING USERNAME: ', err))
-
-        // GET LIGHT DATA WITH USERNAME
-        axios.get(reqURL + '/' + usernameID + '/lights')
-        .then(res => { console.log('res222  ', res.data) })
-    } else { console.error('ERROR: HUE DATA TO GET USERNAME') }
-}
-
-async function setUpHue() {
-    // https://developers.meethue.com/develop/get-started-2/
-    // SET UP A NEW AUTHORIZED USER (NEED TO CLICK ON THE BRIDGE TO START SYNC)
-
-    // GET INTERNAL IP ADDRESS
-
-    // ADD IN JSON OBJECT AS PART OF A POST
-    // {"devicetype":"my_hue_app#android kyle"}
-
-    // ON SUCCESSFUL POST OF NEW USER, STORE RETURNED USERNAME
+	if (!previousData) {
+		console.error('ERROR: NO PREVIOUS USER DATA', previousData);
+	} else {
+		console.log('PREVIOUS DATA CHECK: FINISHED', previousData);
+		await getHueData();
+	}
 }
 
 async function getHueData() {
-    return;
+	const discoveryApp = 'https://discovery.meethue.com';
+	let discoveryData;
+	let hueData = new Object();
+	
+	axios.get(discoveryApp)
+		.then(async res => {
+			discoveryData = res.data;
+		})
+		.then(async () => {
+			if (discoveryData) {
+				console.log('RESPONSE: ', discoveryData);
+
+				// TODO: CHECK TO SEE IF WE HAVE ANY WHITELISTED USERS
+				// GET HUE LIGHTS INFORMATION FROM BRIDGE
+				let usernameID = 'Me78u3OjDQfTnyAdMemLje9-J3uJqyQih-2NZHmL' // TODO: GET RID OF HARD CODED VALUE, CHECK DB FOR USER AND RETURN ID.
+				let getHueSystemData = `http://${discoveryData[0].internalipaddress}/api/${usernameID}`;
+				axios.get(getHueSystemData)
+					.then(res => {
+						if (res && res.status === 200) {
+							// console.log('TTTTTT', res.data);
+							hueData.credentials = {
+								hueId: discoveryData.id,
+								hueBridgeIP: discoveryData.internalipaddress,
+								usernameID: usernameID
+							}
+							hueData.data = res.data;
+							return hueData;
+						} else {
+							console.error('HUE DATA GRAB ERROR: ', res.status, res.statusText);
+							hueData = false;
+							return hueData;
+							// username = await createHueUserName(discoveryData);
+						}
+					})
+					.catch(err => { console.error('ERROR GETTING HUE DATA ', err) })
+
+				return hueData;
+			} else {
+				console.error('ERROR: UNABLE TO GET HUE BRIDGE DATA')
+				return false;
+			}
+		})
+		.catch(err => console.error("ERROR WITH DISCOVERY APP: ", err.status))
+	
+}
+
+// IF WE DO NOT HAVE A USER ASSIGNED YET, WE NEED TO CREATE ONE
+async function createHueUserName(hueData) {
+	console.log('gggggg', hueData)
+	let usernameID = 'Me78u3OjDQfTnyAdMemLje9-J3uJqyQih-2NZHmL' // aGbX60RgWmgCVTf9BRevBfcZDmjjZ9xd1eow4F8V
+	if (hueData[0] && hueData[0].internalipaddress) {
+		let reqURL = `http://${hueData[0].internalipaddress}/api`;
+
+		// axios.get(reqURL)
+		//     .then(res=> {console.log('res  ', res)})
+		//     .catch(err=> console.error('ERROR GETTING USERNAME: ', err))
+
+		let data = {
+			"devicetype": "my_hue_app#kylelaptop"
+		}
+
+		// // SEND HUE BRIDGE NEW USERNAME
+		// axios.post(reqURL, data)
+		//     .then(res => { 
+		//         console.log('res  ', res.data) 
+		//         if (res.success) {
+		//             console.log('Success!', res);
+		//             usernameID = res.username;
+		//         } else { console.log('USER POST ERROR: ', res)}
+		//     })
+		//     .catch(err => console.error('ERROR GETTING USERNAME: ', err))
+
+		// GET LIGHT DATA WITH USERNAME
+		axios.get(reqURL + '/' + usernameID + '/lights')
+			.then(res => { console.log('res222  ', res.data) })
+	} else { console.error('ERROR: HUE DATA TO GET USERNAME') }
+}
+
+async function setUpHue() {
+	// https://developers.meethue.com/develop/get-started-2/
+	// SET UP A NEW AUTHORIZED USER (NEED TO CLICK ON THE BRIDGE TO START SYNC)
+
+	// GET INTERNAL IP ADDRESS
+
+	// ADD IN JSON OBJECT AS PART OF A POST
+	// {"devicetype":"my_hue_app#android kyle"}
+
+	// ON SUCCESSFUL POST OF NEW USER, STORE RETURNED USERNAME
 }
 
 async function checkForProfileData() {
-    return true;
+	return true;
+
 }
 
 async function controlLights() {
-    console.log('Control Lighting Function');
+	console.log('Control Lighting Function');
+	if (!lightingData) { console.error('ERROR: NO LIGHTING DATA AVAILABLE') }
+	else {
+		inquirer.prompt({
+			name: 'controlLightsHome',
+			type: listType,
+			message: 'Lighting: What Would You Like To Do?',
+			choices: [
+				'Toggle Lights On/Off',
+				'Change Colors',
+				'Back'
+			]
+		})
+			.then(response => {
+				switch (response.controlLightsHome) {
+					case 'Toggle Lights On/Off':
+						controlHueLights('onOff')
+						break;
+
+					case 'Change Colors':
+						break;
+
+					case 'Back':
+						break;
+
+					default:
+						console.log('HIT DEFAULT LIGHTING RESPONSE', response)
+						return;
+				}
+			})
+	}
+}
+
+async function controlHueLights(command) {
+	switch (command) {
+		case 'onOff':
+			console.log('TURNED ON/OFF LIGHTS!');
+			if (lightingData){
+				// console.log('OUR LIGHTING DATA: ', lightingData)
+			}
+			break;
+	}
 }
