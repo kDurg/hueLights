58f3f9ebcb926656b81c80e6d7383ab3029028de
Kyle Durigan <kyle.durigan@gmail.com>
2020-10-17 19:37:51 -0600

    progress on lightstate

--- lightinghub/pages/index.js
+++ lightinghub/pages/index.js
@@ -6,10 +6,10 @@ const axios = require('axios');
 
 // GLOBAL TEMPORARY VARIABLES
 let currentLightState;
-let lightingData =[];
+let lightingData = [];
 
 async function checkForProfileData() {
-	return true;
+  return true;
 }
 
 async function controlHueLights(command, modifier) {
@@ -31,15 +31,20 @@ async function controlHueLights(command, modifier) {
               let lightID = light[0];
               let lightState = light[1].state.on; // TODO: CAN THIS BE CLEANED UP BETTER?
               let builtURL = `${coreURL}/lights/${lightID}/state/${lightID}`;
-              lightState ? data = { "on": false } : data = { "on": true };
 
+              lightState ? currentLightState = true : null;
+              currentLightState ? data = {'on': currentLightState} : data = {'on': !currentLightState} // FIXME: NOT TOGGLING CORRECTLY HERE OR ABOVE?
+
+              console.log('LIGHT STATUS: ', currentLightState)
               axios.put(builtURL, data)
                 .then(res => { console.log('SUCCESS RESPONSE', res.data) })
                 .catch(err => { console.error('ERROR CHANGING LIGHT STATUS ', err) })
             });
           }
+
+
         } else { console.error('ERROR, NO LIGHTING DATA', hueData) }
-      break;
+        break;
 
       case 'changeColors':
         if (hueData && hueData.data && hueData.credentials && hueData.credentials.hueCoreURL && modifier) {
@@ -49,8 +54,8 @@ async function controlHueLights(command, modifier) {
               let lightID = light[0];
               let lightState = light[1].state.on; // TODO: CAN THIS BE CLEANED UP BETTER?
               let builtURL = `${coreURL}/lights/${lightID}/state/${lightID}`;
-              
-              if (modifier && lightState){
+
+              if (modifier && lightState) {
                 let lightingData = modifier;
               }
 
@@ -61,98 +66,101 @@ async function controlHueLights(command, modifier) {
           }
           else { console.error('ERROR NO LIGHTS FOR COLOR CHANGE') }
         }
-      break;
+        break;
     }
   }
 }
 
 async function getHueData() {
-  	const discoveryApp = 'https://discovery.meethue.com';
-	let discoveryData;
-	let hueData = new Object();
+  const discoveryApp = 'https://discovery.meethue.com';
+  let discoveryData;
+  let hueData = new Object();
 
-	return axios.get(discoveryApp)
-		.then(async res => {
-			discoveryData = res.data;
-		})
-		.then(async () => {
-			if (discoveryData) {
-				// console.log('RESPONSE: ', discoveryData);
+  return axios.get(discoveryApp)
+    .then(async res => {
+      discoveryData = res.data;
+    })
+    .then(async () => {
+      if (discoveryData) {
+        // console.log('RESPONSE: ', discoveryData);
 
-				// TODO: CHECK TO SEE IF WE HAVE ANY WHITELISTED USERS
-				// GET HUE LIGHTS INFORMATION FROM BRIDGE
-				let usernameID = 'Me78u3OjDQfTnyAdMemLje9-J3uJqyQih-2NZHmL'; // TODO: GET RID OF HARD CODED VALUE, CHECK DB FOR USER AND RETURN ID.
-				let getHueSystemData = `http://${discoveryData[0].internalipaddress}/api/${usernameID}`;
-				return axios.get(getHueSystemData)
-					.then(res => {
-						if (res && res.status === 200) {
-							hueData.credentials = {
-								hueCoreURL: getHueSystemData,
-								hueId: discoveryData[0].id,
-								hueBridgeIP: discoveryData[0].internalipaddress,
-								usernameID: usernameID
-							}
-							hueData.data = res.data;
-							return hueData;
-						} else {
-							console.error('HUE DATA GRAB ERROR: ', res.status, res.statusText);
-							hueData = false;
-							return hueData;
-							// username = await createHueUserName(discoveryData);
-						}
-					})
-					.catch(err => { console.error('ERROR GETTING HUE DATA ', err) })
-			} else {
-				console.error('ERROR: UNABLE TO GET HUE BRIDGE DATA')
-				return false;
-			}
-		})
-		.catch(err => console.error("ERROR WITH DISCOVERY APP: ", err.status))
+        // TODO: CHECK TO SEE IF WE HAVE ANY WHITELISTED USERS
+        // GET HUE LIGHTS INFORMATION FROM BRIDGE
+        let usernameID = 'Me78u3OjDQfTnyAdMemLje9-J3uJqyQih-2NZHmL'; // TODO: GET RID OF HARD CODED VALUE, CHECK DB FOR USER AND RETURN ID.
+        let getHueSystemData = `http://${discoveryData[0].internalipaddress}/api/${usernameID}`;
+        return axios.get(getHueSystemData)
+          .then(res => {
+            if (res && res.status === 200) {
+              hueData.credentials = {
+                hueCoreURL: getHueSystemData,
+                hueId: discoveryData[0].id,
+                hueBridgeIP: discoveryData[0].internalipaddress,
+                usernameID: usernameID
+              }
+              hueData.data = res.data;
+              return hueData;
+            } else {
+              console.error('HUE DATA GRAB ERROR: ', res.status, res.statusText);
+              hueData = false;
+              return hueData;
+              // username = await createHueUserName(discoveryData);
+            }
+          })
+          .catch(err => { console.error('ERROR GETTING HUE DATA ', err) })
+      } else {
+        console.error('ERROR: UNABLE TO GET HUE BRIDGE DATA')
+        return false;
+      }
+    })
+    .catch(err => console.error("ERROR WITH DISCOVERY APP: ", err.status))
 
 }
 
 async function getLightingData() {
-	const previousData = await checkForProfileData();
+  const previousData = await checkForProfileData();
 
-	// PHILIPS HUE
-	// DISCOVER LOCAL DEVICES ON NETWORK
+  // PHILIPS HUE
+  // DISCOVER LOCAL DEVICES ON NETWORK
 
-	if (!previousData) {
-		console.error('ERROR: NO PREVIOUS USER DATA', previousData);
-	} else {
-		console.log('PREVIOUS DATA CHECK: FINISHED', previousData);
-		await getHueData()
-			.then(data => { lightingData.push(data) }) // FIXME: NOT ADDING TO THE ARRAY?
+  if (!previousData) {
+    console.error('ERROR: NO PREVIOUS USER DATA', previousData);
+  } else {
+    console.log('PREVIOUS DATA CHECK: FINISHED', previousData);
+    await getHueData()
+      .then(data => { lightingData.push(data) }) // FIXME: NOT ADDING TO THE ARRAY?
     console.log('LIGHTING DATA', lightingData)
-    if (lightingData){
-      await getLightOnOffStatus()
+    if (lightingData) {
+      // await getLightOnOffStatus()
     }
-
-	}
+  }
 }
 
-async function toggleAllLights(){
+async function toggleAllLights() {
   return await controlHueLights('onOff')
 }
 
-async function getLightOnOffStatus(){
-  if (lightingData && lightingData[0] && lightingData[0].lights){
+async function getLightOnOffStatus() {
+  if (lightingData && lightingData[0] && lightingData[0].lights) {
     let lights = lightingData[0].lights;
     return (
       console.log('OUR LGIHTS', lights)
     )
-  } else { console.log('no lighting data')}
+  } else { console.log('no lighting data') }
 }
 
-export async function getStaticProps(context){
-  return {props:{}}
-}
+// export async function getStaticProps(context) {
+//   return {
+//     props: {
+
+//     }
+//   }
+// }
 
 
-export default async function Home() {
+export default function Home() {
   getLightingData()
 
-  return (   
+  return (
     <div className={styles.container}>
       <Head>
         <title>Lighting Hub</title>
@@ -172,8 +180,8 @@ export default async function Home() {
 
           <Link href="/controlLights" >
             <a className={styles.card}>
-            <h3> Control Lights &rarr;</h3>
-            <p>View and control all lights from status to color</p>
+              <h3> Control Lights &rarr;</h3>
+              <p>View and control all lights from status to color</p>
             </a>
           </Link>
 
