69f83e41b2a94f8721fbea03c2c4a094d33ef734
Kyle Durigan <kyle.durigan@gmail.com>
2020-11-27 09:15:29 -0700

    temp commit

--- lightinghub/LightingHubs/hue.js
+++ lightinghub/LightingHubs/hue.js
@@ -1,3 +1,10 @@
-export default function hueController(data) {
+export default function hueController(modifier, data) {
+    if (modifier, data){
+        switch (modifier){
+
+            
+            default: return 
+        }
+    }
     return console.log('YAYAYAYYAYA', data);
 }
